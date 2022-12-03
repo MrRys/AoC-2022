7b2d538c42ba3252637728cf339f1451bc0bf850
Letanec <richard.letanec1@gmail.com>
2022-12-03 20:37:07 +0100

    Update d3.py

--- d3/d3.py
+++ d3/d3.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import functools
 
 
 def load_input(src: str = None) -> str:
@@ -8,33 +9,26 @@ def load_input(src: str = None) -> str:
 
 
 def parse_rucksacks(input: str) -> list[str]:
-    return input.split("\n")
+    return list(
+        map(
+            lambda rucksack: [rucksack[: len(rucksack) // 2], rucksack[len(rucksack) // 2 :]],
+            input.split("\n"),
+        )
+    )
 
 
 def parse_groups(input: str) -> list[list[str]]:
     return list(map(lambda group: group.strip().split("\n"), re.findall(r".+\n.+\n.+\n?", input)))
 
 
-def part1(rucksacks: list[str]) -> int:
+def solve(groups: list[list[str]]) -> int:
     return sum(
         map(
             lambda i: ord(i) - ord("a") + 1 if i.islower() else ord(i) - ord("A") + 27,
             map(
-                lambda rucksack: list(
-                    set(rucksack[: len(rucksack) // 2]) & set(rucksack[len(rucksack) // 2 :])
+                lambda group: list(
+                    functools.reduce(lambda itemsA, itemsB: set(itemsA) & set(itemsB), group)
                 )[0],
-                rucksacks,
-            ),
-        )
-    )
-
-
-def part2(groups: list[list[str]]) -> int:
-    return sum(
-        map(
-            lambda i: ord(i) - ord("a") + 1 if i.islower() else ord(i) - ord("A") + 27,
-            map(
-                lambda group: list(set(group[0]) & set(group[1]) & set(group[2]))[0],
                 groups,
             ),
         )
@@ -45,5 +39,5 @@ if __name__ == "__main__":
     input = load_input()
     rucksacks = parse_rucksacks(input)
     groups = parse_groups(input)
-    print(part1(rucksacks))
-    print(part2(groups))
+    print(solve(rucksacks))
+    print(solve(groups))
