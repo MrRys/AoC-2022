ad531daed75179313d44486b1d8347f73811957b
MrRys <richard.letanec@gmail.com>
2022-12-08 22:03:04 +0100

    Update d4.py

--- d4/d4.py
+++ d4/d4.py
@@ -7,7 +7,7 @@ def load_input(src: str = None) -> str:
         return src_file.read().strip()
 
 
-def parse_section_pairs(input: str) -> list[list[int]]:
+def parse_elf_pairs(input: str) -> list[list[int]]:
     return list(map(lambda x: list(map(lambda y: int(y), re.split(r",|-", x))), input.split("\n")))
 
 
@@ -16,11 +16,11 @@ def check_overlap(pair: list[int], idx: list[int]) -> bool:
 
 
 def solve(pairs: list[list[int]], idx: list[int]):
-    return sum(map(lambda x: check_overlap(x, idx), pairs))
+    return sum(map(lambda pair: check_overlap(pair, idx), pairs))
 
 
 if __name__ == "__main__":
     input = load_input()
-    pairs = parse_section_pairs(input)
+    pairs = parse_elf_pairs(input)
     print(solve(pairs, [0, 2, 1, 3]))
     print(solve(pairs, [0, 3, 1, 2]))
