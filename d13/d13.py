269c813b595b6844c39273f610b8e22e9400b147
MrRys <richard.letanec@gmail.com>
2022-12-13 21:08:10 +0100

    Update d13.py

--- d13/d13.py
+++ d13/d13.py
@@ -11,31 +11,31 @@ def parse_list_pairs(input: str) -> list[list]:
     return [[literal_eval(line) for line in pair.split("\n")] for pair in input.split("\n\n")]
 
 
-def check_pair(left, right) -> int:
+def compare_pair(left, right) -> int:
     if isinstance(left, int) and isinstance(right, int):
         return -(left - right)
 
     elif isinstance(left, list) and isinstance(right, list):
         for left_n, right_n in zip(left, right):
-            result = check_pair(left_n, right_n)
+            result = compare_pair(left_n, right_n)
             if result != 0:
                 return result
         return -(len(left) - len(right))
 
     else:
-        return check_pair([left], right) if isinstance(left, int) else check_pair(left, [right])
+        return compare_pair([left], right) if isinstance(left, int) else compare_pair(left, [right])
 
 
 def part1(pairs: list[list]) -> int:
     result = 0
     for pair_n, pair in enumerate(pairs):
-        result += (check_pair(pair[0], pair[1]) > 0) * (pair_n + 1)
+        result += (compare_pair(pair[0], pair[1]) > 0) * (pair_n + 1)
     return result
 
 
 def part2(pairs: list[list]) -> int:
     flatten = [element for pair in pairs for element in pair]
-    sorted_list = sorted(flatten + [[[2]], [[6]]], key=cmp_to_key(check_pair), reverse=True)
+    sorted_list = sorted(flatten + [[[2]], [[6]]], key=cmp_to_key(compare_pair), reverse=True)
     return (sorted_list.index([[2]]) + 1) * (sorted_list.index([[6]]) + 1)
 
 
